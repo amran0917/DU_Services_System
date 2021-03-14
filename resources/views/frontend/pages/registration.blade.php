7a3affae4b2a476d3da89fd86550c49f4b0e7a64
amran0917 <bsse0917@iit.du.ac.bd>
2021-03-15 00:22:31 +0600

    change log in photo admin

--- resources/views/frontend/pages/registration.blade.php
+++ resources/views/frontend/pages/registration.blade.php
@@ -172,9 +172,8 @@
                 data    : form_data.serialize(),
                 
                 success : function(data){
-                    // alert('success');
-
-                             $('#myModal').modal('show');
+                  
+                        $('#myModal').modal('show');
                             console.log('success');
                   
                         $("#applicaId").text(data.applicant_id);
@@ -182,7 +181,7 @@
                 },
 
                 error: function (xhr) {
-                    // alert('error');
+                    alert('Error in data Input');
                     console.log(xhr);
 
                         $('#error_name').html('');
@@ -221,27 +220,5 @@
             </div>
 </div>
 
-{{-- <div class="modal fade text-center py-5 subscribeModal-lg"  id="errormodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-				<div class="top-strip"></div>
-                <a class="h2" href="https://www.fiverr.com/share/qb8D02" target="_blank">Sunlimetech</a>
-                <h3 class="pt-5 mb-0 text-secondary">Newsletter</h3>
-                <p class="pb-1 text-muted"><small>Sign up to update with our latest news and products.</small></p>
-                <form>
-                    <div class="input-group mb-3 w-75 mx-auto">
-    				  <input type="email" class="form-control" placeholder="sunlimetech@gmail.com" aria-label="Recipient's username" aria-describedby="button-addon2" required>
-    				  <div class="input-group-append">
-    					<button class="btn btn-primary" type="button" id="button-addon2"><i class="fa fa-paper-plane"></i></button>
-    				  </div>
-    				</div>
-    			</form>
-                <p class="pb-1 text-muted"><small>Your email is safe with us. We won't spam.</small></p>
-				<div class="bottom-strip"></div>
-            </div>
-        </div>
-    </div>
-</div> --}}
-    
+
 @endsection
