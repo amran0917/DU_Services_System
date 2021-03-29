ecf3d306a016707da1d20c59249cc22789be088b
amran0917 <bsse0917@iit.du.ac.bd>
2021-03-29 12:26:43 +0600

    cache cleared

--- resources/views/admin/pages/partials/certificate.blade.php
+++ resources/views/admin/pages/partials/certificate.blade.php
@@ -1,3 +1,22 @@
+
+<div style="width:800px; height:600px; padding:20px; text-align:center; border: 10px solid #787878">
+<div style="width:750px; height:550px; padding:20px; text-align:center; border: 5px solid #787878">
+       <span style="font-size:50px; font-weight:bold">Certificate of Completion</span>
+       <br><br>
+       <span style="font-size:25px"><i>This is to certify that</i></span>
+       <br><br>
+       <span style="font-size:30px"><b>{{ $stdnt->name}}</b></span><br/><br/>
+       <span style="font-size:25px"><i>has completed the course</i></span> <br/><br/>
+       <span style="font-size:30px">$course.getName()</span> <br/><br/>
+       <span style="font-size:20px">with score of <b>{{$allstdnt->cgpa}} </b></span> <br/><br/><br/><br/>
+       <span style="font-size:25px"><i>dated</i></span><br>
+      #set ($dt = $DateFormatter.getFormattedDate($grade.getAwardDate(), "MMMM dd, yyyy"))
+      <span style="font-size:30px">$dt</span>
+</div>
+</div
+
+
+{{-- 
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,4 +61,4 @@
         }}
     </div>
 </body>
-</html>
+</html> --}}
